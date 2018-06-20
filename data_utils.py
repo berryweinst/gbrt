ac8df12b63ae8174d4cf08c3a0d7a5c82174b6a3
Damian <pereldamian@gmail.com>
2018-06-20 13:27:23 +0300

    rec

--- data_utils.py
+++ data_utils.py
@@ -12,13 +12,13 @@ def split_data(data, train_percent=0.8):
     return train, test
 
 
-def parse_data(path, label_name='SalePrice'):
+def parse_data(path, label_name='SalePrice', train_percent=0.8):
     df = pd.read_csv(path, index_col=0)
     df = df[~df[label_name].isnull()]
     if 'MSSubClass' in df.columns:
         df['MSSubClass'] = df.MSSubClass.astype(object)  # categorical feature with numerical values
 
-    df_train, df_test = split_data(df)
+    df_train, df_test = split_data(df, train_percent=train_percent)
 
     train_dataset = TrainDataSet(df_train, label_name=label_name)
     test_dataset = TestDataSet(df_test, train_dataset.categorical_columns_coding_map,
